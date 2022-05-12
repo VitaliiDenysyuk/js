d1dfb3a82f1cabb898f0a5b84a3e0603d7dfd062
Vitalii Denysyuk <denvit1@gmail.com>
2022-05-12 23:28:10 +0300

    add comments to task 1

--- JS_task_1.js
+++ JS_task_1.js
@@ -1,6 +1,9 @@
 const inputText = "Highly recommend you to read at least one work of Shakespeare. He made his masterpieces for hundreds of years before London, Dickens and Fitzgerald. Hope to buy last book to my collection it 2022, and read it.";
 
 const parser = (txt) => {
+  //use (?<=pattern)	Positive Lookbehind 
+  //search some quantity letters witch started at big  letter
+  //and before it present "not dot + space"
   return txt.replace(/(?<=[^\.]\s)[A-Z]\w+/g,"writer")
 };
 
